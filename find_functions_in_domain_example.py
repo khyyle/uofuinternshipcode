9305755e4471786e812913adac13f41b8301b66f
kyleberkson <kyleberkson@gmail.com>
2023-07-24 14:31:26 -0600

    added error handling

--- find_functions_in_domain_example.py
+++ find_functions_in_domain_example.py
@@ -1,14 +1,32 @@
 import apifunctions as myfunc
 
-#example implementation
 stidlist = myfunc.find_stations_in_region()
+print()
+print(f"{len(stidlist)} stations found:")
 print(stidlist)
 
 start = input("enter a starting date and time to pull wind data from (YYYYmmddHHMM):")
 end = input("enter a end date and time to pull wind data from (YYYYmmddHHMM):")
 choice = input(f"would you like to save data for {len(stidlist)} stations? (Y/N):")
+folder_name = input("Enter a folder name to save the XML files in:")
+length = len(stidlist)
 
 for station in stidlist:
     result = myfunc.apirequest(station, start, end)
-    myfunc.save_to_xml(result, choice)
+
+    #omit station if the current station has no data or is inaccessible
+    try:
+        myfunc.save_to_xml(result, choice, folder_name)
+    except KeyError:
+        length -=1
+        print()
+        print(f"no data available for station: {station}")
+        print(f"a total of {length} xml files will now be created")
+        continue 
+    except IndexError:
+        length -= 1
+        print()
+        print(f"no data available for station: {station}")
+        print(f"a total of {length} xml files will now be created")
+        continue 
 
--- weatherstationfinderv2.py
+++ weatherstationfinderv2.py
@@ -7,16 +7,26 @@ print(stid)
 start = input("Enter a starting date and time to pull wind data from (YYYYmmddHHMM):")
 end = input("Enter a end date and time to pull wind data from (YYYYmmddHHMM):")
 choice = input(f"would you like to save data for {len(stid)} stations? (Y/N):")
+folder_name = input("Enter a folder name to save the XML files in:")
 
+length = len(stid)
 #iterate through the list of station ids and pull data for each one 
 for station in stid:
     result = myfunc.apirequest(station, start, end)
-    
-    #save to an xml file
-    myfunc.save_to_xml(result, choice)
-    
 
-
-
-
-
+    try:
+        myfunc.save_to_xml(result, choice, folder_name)
+    except KeyError:
+        length -= 1
+        print() 
+        print(f"no data available for station: {station}")
+        print(f"now outputting data for {length} stations")
+        continue 
+    except IndexError:
+        length -= 1
+        print()
+        print(f"no data available for station: {station}")
+        print(f"now outputting data for {length} stations")
+        continue 
+         
+        
