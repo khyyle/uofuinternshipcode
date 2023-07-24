9db19f16b208c94a6280342ba46a55de9279359c
kyleberkson <kyleberkson@gmail.com>
2023-07-23 22:36:34 -0400

    interface with changes made to apifunctions.py

--- find_functions_in_domain_example.py
+++ find_functions_in_domain_example.py
@@ -3,3 +3,12 @@ import apifunctions as myfunc
 #example implementation
 stidlist = myfunc.find_stations_in_region()
 print(stidlist)
+
+start = input("enter a starting date and time to pull wind data from (YYYYmmddHHMM):")
+end = input("enter a end date and time to pull wind data from (YYYYmmddHHMM):")
+choice = input(f"would you like to save data for {len(stidlist)} stations? (Y/N):")
+
+for station in stidlist:
+    result = myfunc.apirequest(station, start, end)
+    myfunc.save_to_xml(result, choice)
+
--- weatherstationfinderv2.py
+++ weatherstationfinderv2.py
@@ -2,17 +2,18 @@ import apifunctions as myfunc
  
 #determine latitude, longitude, and whether a station id is being used or not
 stid = myfunc.determine_entry()
+print(stid)
 
-#api response
 start = input("Enter a starting date and time to pull wind data from (YYYYmmddHHMM):")
 end = input("Enter a end date and time to pull wind data from (YYYYmmddHHMM):")
+choice = input(f"would you like to save data for {len(stid)} stations? (Y/N):")
 
 #iterate through the list of station ids and pull data for each one 
 for station in stid:
     result = myfunc.apirequest(station, start, end)
     
     #save to an xml file
-    myfunc.save_to_xml(result)
+    myfunc.save_to_xml(result, choice)
     
 
 
