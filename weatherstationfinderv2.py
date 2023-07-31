8bf663d073c8864e9c86ae012f7222e73e473bbb
kyleberkson <kyleberkson@gmail.com>
2023-07-31 15:30:34 -0600

    interpolates values now

--- weatherstationfinderv2.py
+++ weatherstationfinderv2.py
@@ -6,7 +6,7 @@ stid = myfunc.determine_entry()
 print(stid)
 
 start = "202307141000"#input("Enter a starting date and time to pull wind data from (YYYYmmddHHMM):") # 
-end =  "202307151000" #input("Enter a end date and time to pull wind data from (YYYYmmddHHMM):") 
+end =  "202307141200" #input("Enter a end date and time to pull wind data from (YYYYmmddHHMM):") 
 choice =  "Y"#input(f"would you like to save data for {len(stid)} stations? (Y/N):") 
 folder_name =  "test" #input("Enter a folder name to save the XML files in:")
 interval = int(input("enter desired interval for wind speeds and directions (in full minutes):"))
@@ -14,36 +14,23 @@ interval = int(input("enter desired interval for wind speeds and directions (in 
 length = len(stid)
 #iterate through the list of station ids and pull data for each one 
 for station in stid:
-    result = myfunc.apirequest(interval, station, start, end)
+    result = myfunc.apirequest(station, start, end)
     
-    windspeedsinterp, interp_time_set, oldspeeds, oldtime, winddirecinterp, old_wind_direction  = myfunc.interpolate_wind_speed(station, start, end)
+    windspeedsinterp, interp_time_set, oldspeeds, oldtime, winddirecinterp, old_wind_direction  = myfunc.interpolate_wind_speed(interval, station, start, end)
     
-    plt.plot(oldtime, old_wind_direction, label= "old values", marker = 'o', linestyle = '-', color='blue')
-    plt.plot(interp_time_set, winddirecinterp, label="new values", marker = "x", linestyle='-', color= 'red')
+    plt.plot(oldtime, old_wind_direction, label= "old values (direction)", marker = 'o', linestyle = '-', color='blue')
+    plt.plot(interp_time_set, winddirecinterp, label="new values (direction)", marker = "x", linestyle='-', color= 'red')
     
     plt.xlabel('Time')
     plt.ylabel('Wind direction')
     plt.title('wind directions graphed')
     plt.grid(True)
 
-    plt.savefig('interp_comparison_multiplestations.pdf')
+    plt.savefig('interp_comparison_WBB.pdf')
 
     plt.legend()
 
     plt.show()
-
-    '''
-    plt.plot(oldtime, oldspeeds, marker = 'o', linestyle = '-')
-
-    plt.xlabel('Time')
-    plt.ylabel('Wind speed')
-    plt.title("old wind speeds graphed")
-    plt.grid(True)
-
-    plt.savefig('(normalstation)windspeeds_old.pdf')
-
-    plt.show()
-    '''
 
     try:
         myfunc.save_to_xml(result, choice, folder_name)
